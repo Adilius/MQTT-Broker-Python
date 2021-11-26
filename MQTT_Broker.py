cc381fea2ec85d0977a80fdecbfad3e906bdba18
Adilius <leugue-gamer@hotmail.com>
2021-11-26 13:58:00 +0100

    broker file

---
+++ MQTT_Broker.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+from threading import Thread
+import json
+import os
+
+def main():
+    print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n hello xilphex \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
